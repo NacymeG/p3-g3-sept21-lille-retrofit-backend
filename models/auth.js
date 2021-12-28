3c26bba723181e7b41ff090357019c8accfc2db3
NacymeG <nacyme.gallel@gmail.com>
2021-12-28 15:23:58 +0100

    Added email availability and  validation with Joi

--- models/auth.js
+++ models/auth.js
@@ -1,32 +1,58 @@
-const connection = require('../db-config');
-const db = connection.promise();
 const argon2 = require('argon2');
+const Joi = require('joi');
+const connection = require('../db-config');
+
+const db = connection.promise();
+
 const hashingOptions = {
   type: argon2.argon2id,
   memoryCost: 2 ** 16,
   timeCost: 5,
   parallelism: 1,
 };
+//= =================== Validation ============================
 
-//=================== Sign up =========================
-const createUser = ({ firstname, lastname, hashPwd, mail}) => {
-  return db.query(`INSERT INTO user (firstname, lastname, password, mail) VALUES (?,?,?,?)`,
-    [firstname, lastname, hashPwd, mail])
+const validate = (data, forCreation = true) => {
+  const presence = forCreation ? 'required' : 'optional';
+  return Joi.object({
+    mail: Joi.string().email().max(128).presence(presence),
+    password: Joi.string().max(64).presence(presence),
+    firstname: Joi.string().min(1).max(64).presence(presence),
+    lastname: Joi.string().min(1).max(64).presence(presence),
+  }).validate(data, { abortEarly: false }).error;
 };
 
+//= ================== Sign up =========================
+
+const credidentialCheck = async ({ mail }) => {
+  const rows = await db.query(`SELECT id FROM user WHERE mail = ?`, [mail]);
+  // console.log('Allo :', rows[0][0].id);
+  return rows[0][0];
+};
+
+const createUser = ({ firstname, lastname, hashPwd, mail }) => {
+  return db.query(
+    `INSERT INTO user (firstname, lastname, password, mail) VALUES (?,?,?,?)`,
+    [firstname, lastname, hashPwd, mail]
+  );
+};
 
 const hashPassword = (plainPassword) => {
   return argon2.hash(plainPassword, hashingOptions);
 };
-//=====================================================
-//=================== Log in ==========================
+//= ====================================================
+//= ================== Log in ==========================
 
 const verifyPassword = (plainPassword, hashedPassword) => {
   return argon2.verify(hashedPassword, plainPassword, hashingOptions);
 };
 
-//=====================================================
+//= ====================================================
 
 module.exports = {
-  createUser,verifyPassword,hashPassword,
+  createUser,
+  verifyPassword,
+  hashPassword,
+  credidentialCheck,
+  validate,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,24 @@
 const authRouter = require('express').Router();
 const Auth = require('../models/auth');
 
+const errorMessage = {
+  mail: 'Email already exist',
+};
+
 authRouter.post('/signup', async (req, res) => {
-  const { firstname, lastname, mail, password } = req.body;  
-  const hashPwd = await Auth.hashPassword(password)
-    await Auth.createUser({ firstname, lastname, hashPwd, mail})
-    res.status(201).send('Signup success');
+  const validationErrors = await Auth.validate(req.body);
+  if (validationErrors) {
+    res.status(409).send(validationErrors.details[0].message);
+    throw new Error('Validation ERROR');
+  }
+
+  const { firstname, lastname, mail, password } = req.body;
+  const isMail = await Auth.credidentialCheck({ mail });
+  if (isMail) res.status(409).send(errorMessage.mail);
+
+  const hashPwd = await Auth.hashPassword(password);
+  await Auth.createUser({ firstname, lastname, hashPwd, mail });
+  res.status(201).send('Signup success');
 });
 
 module.exports = authRouter;
