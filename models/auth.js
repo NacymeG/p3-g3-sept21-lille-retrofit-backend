c9227aa4c8f40d9f31ff9938947a34d27f8238cf
NacymeG <nacyme.gallel@gmail.com>
2022-01-05 12:09:28 +0100

    Added jwt handling and login query

--- models/auth.js
+++ models/auth.js
@@ -1,6 +1,9 @@
 const argon2 = require('argon2');
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
 const connection = require('../db-config');
+
+const { PRIVATE_KEY } = process.env;
 
 const db = connection.promise();
 
@@ -10,23 +13,33 @@ const hashingOptions = {
   timeCost: 5,
   parallelism: 1,
 };
+
 //= =================== Validation ============================
 
-const validate = (data, forCreation = true) => {
+const signupValidation = (data, forCreation = true) => {
   const presence = forCreation ? 'required' : 'optional';
   return Joi.object({
     mail: Joi.string().email().max(128).presence(presence),
-    password: Joi.string().max(64).presence(presence),
+    password: Joi.string()
+      .max(64)
+      .presence(presence)
+      .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
+    repeadPass: Joi.ref('password'),
     firstname: Joi.string().min(1).max(64).presence(presence),
     lastname: Joi.string().min(1).max(64).presence(presence),
   }).validate(data, { abortEarly: false }).error;
 };
-
+const loginValidation = (data, forCreation = true) => {
+  const presence = forCreation ? 'required' : 'optional';
+  return Joi.object({
+    mail: Joi.string().email().presence(presence),
+    password: Joi.string().presence(presence),
+  }).validate(data, { abortEarly: false }).error;
+};
 //= ================== Sign up =========================
 
 const credidentialCheck = async ({ mail }) => {
   const rows = await db.query(`SELECT id FROM user WHERE mail = ?`, [mail]);
-  // console.log('Allo :', rows[0][0].id);
   return rows[0][0];
 };
 
@@ -40,11 +53,28 @@ const createUser = ({ firstname, lastname, hashPwd, mail }) => {
 const hashPassword = (plainPassword) => {
   return argon2.hash(plainPassword, hashingOptions);
 };
-//= ====================================================
+
 //= ================== Log in ==========================
+const verifyEmail = async (mail) => {
+  const [rows] = await db.query(
+    `SELECT id, mail, password, firstname, lastname FROM user WHERE mail = ?`,
+    [mail]
+  );
+  return rows[0];
+};
 
 const verifyPassword = (plainPassword, hashedPassword) => {
   return argon2.verify(hashedPassword, plainPassword, hashingOptions);
+};
+
+//= ==================== JWT =================================
+
+const decode = (jwtToken) => {
+  return jwt.decode(jwtToken);
+};
+
+const calculateToken = (userEmail, userID) => {
+  return jwt.sign({ email: userEmail, user_id: userID }, PRIVATE_KEY);
 };
 
 //= ====================================================
@@ -54,5 +84,9 @@ module.exports = {
   verifyPassword,
   hashPassword,
   credidentialCheck,
-  validate,
+  signupValidation,
+  loginValidation,
+  verifyEmail,
+  decode,
+  calculateToken,
 };
