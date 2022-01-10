baafc6f535d7d8ab2ed584ce1dbccd8022def431
Morel Victor <morel.victor1@gmail.com>
2022-01-10 10:54:38 +0100

    Add team routes

---
+++ routes/team.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+const { db } = require('../db-config');
+
+router.get('/', async (req, res) => {
+  try {
+    const [team] = await db.query(`
+    SELECT firstName, lastName, profilPic, linkedinUrl
+    FROM staff
+  `);
+    res.json(team);
+  } catch (err) {
+    res.status(404).send();
+    console.warn(err);
+  }
+});
+
+module.exports = router;
