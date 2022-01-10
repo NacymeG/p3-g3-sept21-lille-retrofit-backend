f4e9ea8e41c575832c5abcc9b282c9c179435ed7
Morel Victor <morel.victor1@gmail.com>
2022-01-10 12:16:12 +0100

    Add cors middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { backPort } = require('./db-config');
 const teamRoutes = require('./routes/team');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/team', teamRoutes);
 
