3dfcdab6917088f7da868b54befa6f3961dddb5a
NacymeG <nacyme.gallel@gmail.com>
2022-01-10 10:01:51 +0100

    Refarctor of db-config

--- db-config.js
+++ db-config.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST, // address of the server
-  port: process.env.DB_PORT, // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: DB_HOST, // address of the server
+  port: DB_PORT, // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 
 module.exports = connection;
