2cd184b823f64bbac4f86838b26e80e79ef6f1a1
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-13 17:28:20 +0100

    add route localisation

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
-/* const localisation = require('./localisation');
-
-module.exports = { localisation }; */
 const authRouter = require('./auth');
 const teamRoutes = require('./team');
+const localisationRoutes = require('./localisation');
 const homeRoutes = require('./home');
 
 const setupRoutes = (app) => {
   app.use('/auth', authRouter);
   app.use('/team', teamRoutes);
   app.use('/home', homeRoutes);
+  app.use('/localisation', localisationRoutes);
 };
 
 module.exports = {
