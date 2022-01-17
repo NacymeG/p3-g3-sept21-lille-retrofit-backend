378161c4e2db1ddc550a4083c290536131cd160f
NacymeG <nacyme.gallel@gmail.com>
2022-01-17 14:43:37 +0100

    Get cars list modification + added vote route

--- routes/cars.js
+++ routes/cars.js
@@ -3,13 +3,43 @@ const express = require('express');
 const router = express.Router();
 const { db } = require('../db-config');
 
+//= =================== GET all cars ==================
+
 router.get('/', async (req, res) => {
   try {
     const [cars] = await db.query(`
-    SELECT model, image, price 
-    FROM cars 
+    SELECT id, model, image, price, VoteNbr
+    FROM cars ORDER BY VoteNbr DESC
   `);
     res.json(cars);
+  } catch (err) {
+    res.status(404);
+    console.warn(err);
+  }
+});
+
+//= =================== ADD a vote ==================
+
+router.put('/', async (req, res) => {
+  console.log(req.body);
+  const { carId, userId } = req.body;
+  try {
+    const [carsVote] = await db.query('SELECT VoteNbr FROM cars WHERE id = ?', [
+      carId,
+    ]);
+    const [userVote] = await db.query('SELECT vote FROM user WHERE id = ?', [
+      userId,
+    ]);
+
+    const rowIncr = carsVote[0].VoteNbr + 1;
+    const userIncr = userVote[0].vote + 1;
+
+    await db.query('UPDATE cars SET voteNbr = ? WHERE id = ?', [
+      rowIncr,
+      carId,
+    ]);
+    await db.query('UPDATE user SET vote = ? WHERE id = ?', [userIncr, userId]);
+    res.json(rowIncr).status(204);
   } catch (err) {
     res.status(404);
     console.warn(err);
