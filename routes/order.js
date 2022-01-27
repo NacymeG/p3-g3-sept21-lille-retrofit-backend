bbb735ada5095f009cef910095e4998026664f37
Morel Victor <morel.victor1@gmail.com>
2022-01-27 09:53:33 +0100

    Add order routes to display order in user profile

---
+++ routes/order.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.Router();
+const { db } = require('../db-config');
+
+// =========== GET ALL Orders =========== //
+
+router.get('/', async (req, res) => {
+  try {
+    const [order] = await db.query(`
+    SELECT *
+    FROM orders
+    JOIN cars
+    ON cars.id = orders.cars_id
+    JOIN user ON user.id = orders.user_id
+  `);
+    res.json(order).status(201);
+  } catch (err) {
+    res.status(404);
+    console.warn(err);
+  }
+});
+
+module.exports = router;
